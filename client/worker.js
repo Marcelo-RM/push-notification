fd1310a1b711da81ac806d207a2c116896220d20
marcelordrgs98@gmail.com <marcelordrgs98@gmail.com>
2019-12-12 20:20:44 -0300

    final, não funciona

---
+++ client/worker.js
@@ -0,0 +1,10 @@
+console.log("service worker loaded");
+
+self.addEventListener("push", function(event) {
+    const data = event.data.json();
+    console.log("Push recieved...");
+    self.registration.showNotification(data.title, {
+        body: 'Notified by Marcelo',
+        icon: 'http://image.ibb.co/frYOFd/tmlogo.png'
+    });
+});
