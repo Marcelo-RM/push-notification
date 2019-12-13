7a6158a9049c1a381f4a7c8c33e3b66e9b0bbf5d
Marcelo-RM <marcelordrgs98@gmail.com>
2019-12-13 14:25:30 -0200

    doblequotes in string definition

--- client/client.js
+++ client/client.js
@@ -1,20 +1,21 @@
 //import { sendNotification } from "web-push";
 
-const publicVapidKey = 'BMAinI7w6i8C9SxMBurVEZ1qDy8gIx_dpvcqpqdQDB5EfwyyC5prhLDHQ3Fzi9lgjV21A2yTdDJXRHzjbs7dJZw';
+const publicVapidKey =
+    "BMAinI7w6i8C9SxMBurVEZ1qDy8gIx_dpvcqpqdQDB5EfwyyC5prhLDHQ3Fzi9lgjV21A2yTdDJXRHzjbs7dJZw";
 
 // check for service worker
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
     send().catch(err => console.error(err));
 }
 
 // register sw, register push, send push
 async function send() {
     // Register service worker
-    console.log('Registering service worker...');
-    const register = await navigator.serviceWorker.register('worker.js', {
-        scope: '/client/'
+    console.log("Registering service worker...");
+    const register = await navigator.serviceWorker.register("worker.js", {
+        scope: "/client/"
     });
-    console.log('Service worker registered...');
+    console.log("Service worker registered...");
 
     // Register push
     console.log("registering push...");
@@ -25,22 +26,22 @@ async function send() {
     console.log("push registered");
 
     //send push notification
-    console.log('sending push...');
-    await fetch('/subscribe', {
-        method: 'POST',
+    console.log("sending push...");
+    await fetch("/subscribe", {
+        method: "POST",
         body: JSON.stringify(subscription),
         headers: {
-            'content-type': 'application/json'
+            "content-type": "application/json"
         }
     });
-    console.log('Push sent...');
+    console.log("Push sent...");
 }
 
 function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
     const base64 = (base64String + padding)
-        .replace(/\-/g, '+')
-        .replace(/_/g, '/');
+        .replace(/\-/g, "+")
+        .replace(/_/g, "/");
 
     const rawData = window.atob(base64);
     const outputArray = new Uint8Array(rawData.length);
